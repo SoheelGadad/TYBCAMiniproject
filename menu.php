b82b8000367c08124ba1de7107b2f068419f4b7b
SoheelGadad <108049316+SoheelGadad@users.noreply.github.com>
2022-10-23 20:58:50 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Royal catring</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
+</head>
+
+<body>
+    <header>
+        <!--Nav bar-->
+        <?php
+    include('nav/nav.php')
+  ?>
+    </header>
+    <div class="container">
+        <div class="menu_page">
+            <p>
+            <h2>Our Menu</h2>
+            </p><br>
+            <div class="left">
+                <p><img src="image/side1.png" /><strong>WELCOME DRINKS</strong><br />
+                <ol>
+                    <li>Cola </li>
+                    <li>Orange </li>
+                    <li>Limca </li>
+                    <li>Mineral Water Glasses</li>
+                    <li>Espresso Coffee </li>
+                </ol>
+                </p>
+                <p><img src="image/side1.png"><strong>SOUP STATION</strong><br />
+                <ol>
+                    <li>Cream of Tomato</li>
+                    <li>Veg. Sweet Corn </li>
+                    <li>Cream of Mushroom</li>
+                    <li>Hot ‘n’ Sour</li>
+                    <li>Tomato Sorba</li>
+                    <li>Mix Veg. Soup</li>
+                    <li>Cream of Almond </li>
+                </ol>
+
+                </p>
+                <p><img src="image/side1.png"><strong>LIVE ROASTED SNACKS</strong><br />
+                <ol>
+                    <li>Paneer Tikka Masala </li>
+                    <li>Paneer Tikka Kasturi</li>
+                    <li>Paneer Malai Tikka </li>
+                    <li>Pineapple Tikka </li>
+                    <li>Veg. Seekh Kebab</li>
+                    <li>Veg. giloti Kebab</li>
+                </ol>
+                </p>
+                <p><img src="image/side1.png"><strong>ICE-CREAM</strong><br />
+                <ol>
+                    <li>Vanilla</li>
+                    <li>Strawberry </li>
+                    <li>Kaju Kismis</li>
+                    <li>Kesar Pista</li>
+                    <li>Butter Scotch</li>
+                    <li>Chocolate</li>
+                    <li>Tutee Fruity</li>
+                </ol>
+                </p>
+                <p><img src="image/side1.png" /><strong>ORIENTAL SNACKS</strong><br />
+                <ol>
+                    <li>Spring Rolls</li>
+                    <li>Chilly Paneer</li>
+                    <li>Chilly Gobhi</li>
+                    <li>Chilly Crispy Potato</li>
+                    <li>Chilly Mushroom </li>
+                    <li>Veg. Manchurian</li>
+                    <li>Honey Money Veg.</li>
+                    <li>Golden Fried Baby Corn </li>
+                </ol>
+                </p>
+
+                <p><img src="image/side1.png"><strong>SWEET SNACKS</strong><br />
+                <ol>
+                    <li>Tirangi Burfi</li>
+                    <li>Kesar Paak </li>
+                    <li>Chhaina Pineapple Sandes</li>
+                    <li>Chhaine ki Goli</li>
+                    <li>Petha Angoori</li>
+                    <li>Khurchan Roll</li>
+                    <li>Kaju Katli</li>
+                    <li>Kaju Roll</li>
+                    <li>Kaju Kalash</li>
+                    <li>Badam roll </li>
+                    <li>Tirangi Burfi Mewa wali</li>
+                    <li>Pista Lauge</li>
+                    <li>Pista Gilouri </li>
+                </ol>
+
+                </p>
+                <p><img src="image/side1.png"><strong>EXOTIC FRUITS</strong><br />
+                <ol>
+                    <li>Kiwi</li>
+                    <li>Guava</li>
+                    <li>Apple </li>
+                    <li>Strawberry </li>
+                    <li>Mango</li>
+                </ol>
+
+                </p>
+                <p><img src="image/side1.png"><strong>CHATPATI CHAAT</strong><br />
+                <ol>
+                    <li>Gol Gappe </li>
+                    <li>Dahi Bhalla/ Papdi /Kalmi Wada</li>
+                    <li>Aloo Tikki</li>
+                    <li>Paneer Tikki</li>
+                    <li>Matara Lucknawi Tawa</li>
+                    <li>Pav Bhaji</li>
+                    <li>Raj Kachori</li>
+                    <li>Lachchha Tokari</li>
+                    <li>Kathi Kebab Rumali Roti</li>
+                    <li>Dal Muradabadi</li>
+                    <li>Ram Laddoo</li>
+                    <li>Chowmien </li>
+                    <li>Paneer Tikka</li>
+                    <li>Chusky</li>
+                    <li>English Tawa</li>
+                    <li>Fruit Kulia</li>
+                    <li>Dry Fruit Chaat</li>
+                    <li>Lotan ke Chhole</li>
+                </ol>
+                </p>
+            </div>
+            <div class="right">
+                <p><img src="image/side1.png"><strong>MAIN COURSE</strong><br />
+                <ol>
+                    <li>Shahi Paneer</li>
+                    <li>Paneer Pasanda </li>
+                    <li>Paneer Butter Masala </li>
+                    <li>Stuffed Paneer </li>
+                    <li>Kadhai Paneer</li>
+                    <li>Paneer Mughlai</li>
+                    <li>Paneer Malai Kofta</li>
+                    <li>Paneer Aloo-Bukhara Kofta</li>
+                    <li>Matar Makhana Korma</li>
+                    <li>Aloo Dum</li>
+                    <li>Aloo Tamatar</li>
+                    <li>Aloo Kashmiri</li>
+                    <li>Aloo Stuff</li>
+                    <li>Palak Corn</li>
+                    <li>Palak Paneer Kofta</li>
+                    <li>Palak Gobhi</li>
+                    <li>Mix Vegetable</li>
+                    <li>Mirchi Salan</li>
+                    <li>Dal Makhni</li>
+                    <li>Dal Banarasi</li>
+                    <li>Chhole Pindi</li>
+                    <li>Chhole Sunehari</li>
+                    <li>Rajma Masala</li>
+                    <li>Masala Soyabean Chaap</li>
+                    <li>Tawa Sabji (Six type)</li>
+                    <li>Tinda Masala (Seasonal)</li>
+                    <li>Tinda Stuff </li>
+                    <li>Bhindi Masala</li>
+                    <li>Kurkuri Bhindi</li>
+                    <li>Gobhi Fry</li>
+                    <li>Gobhi Korma</li>
+                    <li>Gobhi Shahi</li>
+                    <li>Hari Chholiya Paneer</li>
+                    <li>Sarson Ka Saag</li>
+
+                </ol>
+                </p>
+
+                <p><img src="image/side1.png"><strong>PAPAD</strong><br />
+                <ol>
+                    <li>Aloo Papad </li>
+                    <li>Moong Dal Papad</li>
+                    <li>Urad Dal Papad</li>
+                    <li>Rice Papad </li>
+                    <li>Sabudana Papad </li>
+                    <li>Kali mirch Papad </li>
+                    <li>Chawal Wadi</li>
+                </ol>
+                </p>
+                <p><img src="image/side1.png"><strong>RAITA</strong><br />
+                <ol>
+                    <li>Dahi Pakodi</li>
+                    <li>Dahi Bhalla</li>
+                    <li>Dahi Gujia </li>
+                    <li>Dahi Bhalla Moth ka </li>
+                    <li>Fruit Raita</li>
+                    <li>Mix Veg. Raita </li>
+                    <li>Boondi Raita</li>
+                    <li>Pineapple Raita</li>
+                    <li>Plain Curd</li>
+                </ol>
+                </p>
+                <p><img src="image/side1.png"><strong>BREADS</strong><br />
+                <ol>
+                    <li>Butter Naan </li>
+                    <li>Biscuit Naan</li>
+                    <li>Khandari Naan</li>
+                    <li>Lachchha Parantha</li>
+                    <li>Mirchi ka Parantha</li>
+                    <li>Missi Roti</li>
+                    <li>Palak Puri</li>
+                    <li>Chukundar Puri</li>
+                    <li>Kachori Khasta</li>
+                </ol>
+                </p>
+
+                <p><img src="image/side1.png"><strong>PARANTHA</strong><br />
+                <ol>
+                    <li>Ghobi Parantha </li>
+                    <li>Aloo Parantha</li>
+                    <li>Paneer Parantha</li>
+                    <li>Papad Parantha</li>
+                    <li>Matar Parantha</li>
+                    <li>Masala Parantha</li>
+                </ol>
+                </p>
+
+                <p><img src="image/side1.png"><strong>RICE STATION</strong><br />
+                <ol>
+                    <li>Pulao Navratan Vegetable</li>
+                    <li>Pulao Navratan Meva </li>
+                    <li>Pulao Matar Paneer </li>
+                    <li>Pulao Matar Paneer Kaju </li>
+                    <li>Rice Plain </li>
+                    <li>Rice Jeera </li>
+                    <li>Veg Biryani </li>
+                    <li>Hyderabadi Biryani</li>
+                </ol>
+                </p>
+
+                <p><img src="image/side1.png"><strong>SOUTH INDIAN</strong><br />
+                <ol>
+                    <li>Plain Dosa </li>
+                    <li>Masala Dosa</li>
+                    <li>Paper Dosa</li>
+                    <li>Wada </li>
+                    <li>Idly</li>
+                    <li>Uttapam</li>
+                </ol>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- Footer-->
+    <?php
+include('footer/footer.php')
+?>
+    <script src="index.js"></script>
+</body>
+
+</html>
